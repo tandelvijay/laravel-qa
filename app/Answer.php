ab6ab7919da207eb00927804937cdcec3d847312
tandelvijay <tandelvijay19@gmail.com>
2019-02-21 17:07:14 +0530

    Implementing Displaying answers for Questions

--- app/Answer.php
+++ app/Answer.php
@@ -27,4 +27,8 @@ class Answer extends Model
             $answer->question->save();
         });
     }
+
+    public function getCreatedDateAttribute(){
+        return $this->created_at->diffForHumans();
+    }
 }
