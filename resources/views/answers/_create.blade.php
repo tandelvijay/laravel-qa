6ce3294c971d6b958addc4c100aa9e95bdc7b0f3
tandelvijay <tandelvijay19@gmail.com>
2019-03-01 12:53:39 +0530

    Display answers for questions

---
+++ resources/views/answers/_create.blade.php
@@ -0,0 +1,28 @@
+<div class="row mt-4">
+    <div class="com-md-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="card-title">
+                    <h3>Your Answer</h3>
+                </div>
+
+                <form action="{{route('questions.answers.store', $question->id)}}" method="post">
+                    @csrf()
+                    <div class="form-group">
+                        <textarea class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}" name="body" id="" rows="7"></textarea>
+                        @if($errors->has('body'))
+                            <div class="invalid-feedback">
+                                <strong>{{ $errors->first('body') }}</strong>
+                            </div>
+                        @endif
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-lg btn-outline-primary">submit</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
